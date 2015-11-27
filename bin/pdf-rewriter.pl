f954e3df2181733a8a3a9db0f946bbaa71cb35ce
David Warring <david.warring@gmail.com>
2015-11-27 04:46:20 +0000

    [bin/pdf-rewriter.pl] - tidy doco

--- bin/pdf-rewriter.pl
+++ bin/pdf-rewriter.pl
@@ -8,20 +8,19 @@ use PDF::Writer;
 
 #| rewrite a PDF or FDF  and/or convert to/from JSON
 sub MAIN (
-    Str $pdf-or-json-file-in,    # input PDF, FDF or JSON file (.json extension)
-    Str $pdf-or-json-file-out,   # output PDF, FDF or JSON file (.json extension)
-    Bool :$repair = False,       # bypass and repair index. recompute stream lengths. Handy when
-                                 # when PDF files have been hand-edited.
-    Bool :$rebuild    = False,   # rebuild object tree (renumber, garbage collect and deduplicate objects)
-    Bool :$compress   = False,   # uncompress streams
-    Bool :$uncompress = False,   # compress streams
-    Str  :$password = '';        # owner password for encrypted documents
-    Bool :$dom = False,          # require PDF::DOM
+    Str $pdf-or-json-file-in,    #| input PDF, FDF or JSON file (.json extension)
+    Str $pdf-or-json-file-out,   #| output PDF, FDF or JSON file (.json extension)
+    Bool :$repair = False,       #| bypass and repair index. recompute stream lengths. Handy when
+                                 #| PDF files have been hand-edited.
+    Bool :$rebuild    = False,   #| rebuild object tree (renumber, garbage collect and deduplicate objects)
+    Bool :$compress   = False,   #| uncompress streams
+    Bool :$uncompress = False,   #| compress streams
+    Str  :$password = '';        #| password for encrypted documents
+    Bool :$dom = False,          #| require PDF::DOM
     ) {
 
-    if $dom {
-        require ::('PDF::DOM')
-    }
+    require ::('PDF::DOM')
+	if $dom;
 
     die "conflicting arguments: --compress --uncompress"
         if $compress && $uncompress;
