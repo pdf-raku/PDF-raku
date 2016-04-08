6ab0e4a75307c176c15d2b990bd3536ea729220e
David Warring <david.warring@gmail.com>
2016-04-09 06:38:38 +1200

    more work on utf-16be encoding
    rakudo now handles BOM markers

--- lib/PDF/DAO/TextString.pm
+++ lib/PDF/DAO/TextString.pm
@@ -19,24 +19,22 @@ using either PDFDocEncoding or UTF-16BE with a leading byte-order marker
 =end pod
 
     method new( Str :$value! is copy, Bool :$bom is copy = False, |c ) {
-        if $value ~~ s/^ $<bom>=[\xFE \xFF]// {
-	    # utf-16be is big endian
-	    # rakudo doesn't support this encoding yet
-	    my @be = flat $value.ords.map: -> $a, $b { ($b, $a) };
+        if $value ~~ /^ $<bom>=[\xFE \xFF]/ {
+	    my @be = $value.ords;
             $value = Buf.new(@be).decode('utf-16');
         }
         nextwith( :$value, :$bom, |c );
     }
 
     our sub utf16-encode(Str $str --> Str) {
-         constant BOM = "\xFE\xFF";
+         constant BOM-BE = "\xFE\xFF";
 	 my Str $byte-string = $str.encode("utf-16").map( -> $ord {
                    my $lo = $ord mod 0x100;
                    my $hi = $ord div 0x100;
 		   $hi.chr ~ $lo.chr;
 	 }).join('');
 
-	 BOM ~ $byte-string;
+	 BOM-BE ~ $byte-string;
     }
 
     method content {
